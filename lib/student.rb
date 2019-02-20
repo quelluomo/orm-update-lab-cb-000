b818d28ff397b4edf74723faf895ecc98abfbdec
Matteo Muscas <thatman.j@gmail.com>
2019-02-20 15:05:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,7 @@ class Student
     student.save
     student
   end
- 
+
   def self.find_by_name(name)
     sql = "SELECT * FROM student WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
